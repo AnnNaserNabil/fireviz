04ccfea13570eaec963f086d3c071711cb1f78c2
Ann Naser Nabil <ann.n.nabil@gmail.com>
2025-03-03 01:26:21 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="fireviz",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Ann Naser Nabil",
+    author_email="ann.naser@gmail.com",
     description="A quick and elegant visualization package for data analysis.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/fireviz",
+    url="https://github.com/AnnNaserNabil/fireviz",
     packages=find_packages(),
     install_requires=[
         "pandas",
