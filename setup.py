e79e2b91839bce9ee6de9fa5cc666fa2f1ae43cd
Ann Naser Nabil <ann.n.nabil@gmail.com>
2025-03-03 04:54:01 +0600

    updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fireviz",
-    version="0.1.0",
+    version="0.1.1",
     author="Ann Naser Nabil",
     author_email="ann.naser@gmail.com",
     description="A quick and elegant visualization package for data analysis.",
